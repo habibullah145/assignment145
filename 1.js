1ae63b26e79bd3466dbd49ce68692db75e8f120d
Habibullah Qureshi <102140596+habibullah145@users.noreply.github.com>
2023-12-01 11:26:58 +0500

    Add files via upload

---
+++ 1.js
@@ -0,0 +1,3 @@
+"use strict";
+const personName = "Habibullah";
+console.log(`Hello ${personName}, would you like to learn some typescript today?`);
---
+++ 1.ts
@@ -0,0 +1,2 @@
+const personName: string = "Habibullah";
+console.log(`Hello ${personName}, would you like to learn some typescript today?`);
